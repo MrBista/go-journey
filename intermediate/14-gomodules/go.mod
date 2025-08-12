11a15aa804eb089e779d4b091e1d4dec9bd4a857
MrBista <gustibisman2308@gmail.com>
2025-08-12 20:12:45 +0700

    learn gomodules practice

---
+++ intermediate/14-gomodules/go.mod
@@ -0,0 +1,5 @@
+module github.com/MrBista/go-journey/intermediate/14-gomodules
+
+go 1.21.4
+
+require github.com/MrBista/app-hello v1.0.1 // indirect
--- intermediate/14-gomodules/main.go
+++ intermediate/14-gomodules/main.go
@@ -1,1 +1,12 @@
-package gomodules
+package main
+
+import (
+	"fmt"
+
+	"github.com/MrBista/app-hello/helper"
+)
+
+func main() {
+	sayHelloCall := helper.SayHello("Panggil dari package helper")
+	fmt.Println("Hello " + sayHelloCall)
+}
